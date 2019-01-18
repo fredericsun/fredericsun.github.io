feea79e3a0e2c1f9bff20b0c58c64cabff468159
Yue Sun <ysun276@wisc.edu>
2019-01-17 21:18:56 -0600

    The first commit
    Try move all engineering to Git from file which generated by 'npm run build'

---
+++ precache-manifest.ecc82d1706f115cfe069b5d48b431d34.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = [
+  {
+    "revision": "c4133116466ebc73086778001ef7fc3e",
+    "url": "/static/media/Route 66 Road Sign.c4133116.jpg"
+  },
+  {
+    "revision": "ac57b4ea96263910a9fc",
+    "url": "/static/css/main.0f71f549.chunk.css"
+  },
+  {
+    "revision": "39985ddec6679e92b2f8a7b2fa47688a",
+    "url": "/static/media/Madison.39985dde.jpg"
+  },
+  {
+    "revision": "f438dd8068c914c5ed8d",
+    "url": "/static/js/1.f438dd80.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a170ae6d49b44833fca2248d3cd51e9b",
+    "url": "/static/media/Umass_Yellow.a170ae6d.jpg"
+  },
+  {
+    "revision": "07c797b20388f1680c3ad82f4167f265",
+    "url": "/static/media/Umass_White.07c797b2.jpg"
+  },
+  {
+    "revision": "846279e6c992a05492f0f5a9f10dd6e5",
+    "url": "/static/media/San_Thomas.846279e6.jpg"
+  },
+  {
+    "revision": "6a219d58338328648c7001c17382f902",
+    "url": "/static/media/San_Juan.6a219d58.jpg"
+  },
+  {
+    "revision": "c3aa73843f87b9f0ee185ab1f5a2f292",
+    "url": "/static/media/Route 66 Bottle Soda.c3aa7384.jpg"
+  },
+  {
+    "revision": "ac57b4ea96263910a9fc",
+    "url": "/static/js/main.ac57b4ea.chunk.js"
+  },
+  {
+    "revision": "e8c972188db9044eea860f851143ad1a",
+    "url": "/static/media/Nao.e8c97218.JPG"
+  },
+  {
+    "revision": "22a0dde33bc3f375a65eb254257c64cf",
+    "url": "/static/media/Zhengzhou.22a0dde3.jpeg"
+  },
+  {
+    "revision": "16bfd1a9d4c0fabaaf7b11eaec03e9c3",
+    "url": "/static/media/Yue.16bfd1a9.jpg"
+  },
+  {
+    "revision": "fc8b7c8fa69f9027055881613645d3a5",
+    "url": "/static/media/Umass.fc8b7c8f.jpg"
+  },
+  {
+    "revision": "eb6c15999e20d8985dcbcd0712d069a8",
+    "url": "/static/media/UW.eb6c1599.jpg"
+  },
+  {
+    "revision": "b3bea3fca05869b62ac0896714e957f1",
+    "url": "/static/media/Style.b3bea3fc.png"
+  },
+  {
+    "revision": "acbdf339e00e592c6a7ed8728a336bc5",
+    "url": "/static/media/Help.acbdf339.png"
+  },
+  {
+    "revision": "f438dd8068c914c5ed8d",
+    "url": "/static/css/1.4ecd903f.chunk.css"
+  },
+  {
+    "revision": "873145fb21151426b9f7907d139882d0",
+    "url": "/index.html"
+  }
+];
